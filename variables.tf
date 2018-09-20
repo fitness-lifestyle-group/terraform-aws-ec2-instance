c36a490ba098f1ea7d35003f2d689ce6c3833998
Steven van Rooyen <steven.vanrooyen@fitlg.com>
2018-09-21 00:13:10 +1000

    attempt to make lifecycle ignore changes into a var

--- variables.tf
+++ variables.tf
@@ -128,3 +128,8 @@ variable "cpu_credits" {
   description = "The credit option for CPU usage (unlimited or standard)"
   default     = "standard"
 }
+
+variable "lifecyle_ignore_changes" {
+  description = "Allow individual attributes to be ignored through changes"
+  default     = ["private_ip", "root_block_device", "ebs_block_device"]
+}
